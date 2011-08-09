31460e1bc46bb5f5b8347b8a82124ef219c0fe6f
Stas SUȘCOV <stas@net.utcluj.ro>
2011-08-09 12:28:10 +0300

    Actualizat poze, adăugat pagina de membri

--- 2011/constants.php
+++ 2011/constants.php
@@ -5,8 +5,9 @@ $wikitagline = "Întâlnirea comunităților FLOSS din România";
 $camplocation = "5-6-7 August 2011, Șimon (Bran), Brașov, România";
 $menu = '<li><a href="../2011/" title="Acasă">Despre eveniment</a></li>' .
         '<li><a href="../2011/calea" title="Hai și tu">Cum ajungi</a></li>' .
-        '<li><a href="../2011/participare" title="Participă și tu">Cum participi</a></li>' .
-        '<li><a href="../2011/cazare" title="Stai și tu">Cum stai</a></li>' .
+        '<li><a href="../2011/membri" title="Pe cine vei întâlni">Participanți</a></li>' .
+//        '<li><a href="../2011/participare" title="Participă și tu">Cum participi</a></li>' .
+//        '<li><a href="../2011/cazare" title="Stai și tu">Cum stai</a></li>' .
         '<li><a href="../2011/poze" title="Poze">Poze</a></li>' .
         '<li><a href="../2011/contact" title="Scrie-ne">Contact</a></li>';
 ?>
--- 2011/poze.md
+++ 2011/poze.md
@@ -1,5 +1,3 @@
 FLOSSCamp 2011 în imagini
 =========================
 
-Mai jos găsiți câteva poze făcute la locul de campare pe la sfârșitul lui
-Iulie.
