48cfba3b680fc846f7eb6d0766fffa4a555a5f16
Doru Ilasi <doru@aplix.org>
2012-07-05 08:01:21 +0300

    Update master

--- 2012/constants.php
+++ 2012/constants.php
@@ -2,7 +2,7 @@
 $year = "2012";
 $wikiname = "FLOSSCamp 2012";
 $wikitagline = "Întâlnirea comunităților FLOSS din România";
-$camplocation = "Cândva în vara lui 2012, pe lângă Brașov, România";
+$camplocation = "27 iul - 29 iul 2012 Padina Ursului - Fudațica, România";
 $menu = '<li><a href="../2012/" title="Acasă">Despre eveniment</a></li>' .
         '<li><a href="../2012/calea" title="Hai și tu">Cum ajungi</a></li>' .
         '<!--li><a href="../2012/membri" title="Pe cine vei întâlni">Participanți</a></li-->' .
