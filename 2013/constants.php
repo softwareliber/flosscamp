b7769d1d4beed9e765161c854fc385be53557541
Alex Cucu <alex.cucu@chevah.com>
2013-07-15 17:19:43 +0300

    Adaugat pagina initiala 2013

---
+++ 2013/constants.php
@@ -0,0 +1,13 @@
+<?php
+$year = "2013";
+$wikiname = "FLOSSCamp 2013";
+$wikitagline = "Întâlnirea comunităților FLOSS din România";
+$camplocation = "26 iul - 28 iul 2012 - Undeva în România";
+$menu = '<li><a href="../2013/" title="Acasă">Despre eveniment</a></li>' .
+        '<!--li><a href="../2013/calea" title="Hai și tu">Cum ajungi</a></li>' .
+        '<!--li><a href="../2013/membri" title="Pe cine vei întâlni">Participanți</a></li-->' .
+        '<li><a href="../2013/sondaj" title="Unde ne întâlnim?">Sondaj</a></li-->' .
+        '<!--li><a href="../2013/poze" title="Poze">Poze</a></li>' .
+        '<li><a href="../2013/contact" title="Scrie-ne">Contact</a></li>';
+?>
+
---
+++ 2013/contact.md
@@ -0,0 +1,10 @@
+Scrie-ne!
+=========
+
+Scrie-ne orice idee sau parere ai despre acest proiect.
+
+Ne poți trimite un mesaj la adresa [salut@softwareliber.ro](mailto:salut@softwareliber.ro) sau direct, folosind [formularul de contact de pe pagina  softwareliber.ro](http://softwareliber.ro/contact/).
+
+La fel, dacă ai chef de povești, hai pe IRC la [#gsl@irc.freenode.net](irc://irc.ubuntu.com/gsl) sau folosește [clientul web de pe softwareliber.ro](http://irc.softwareliber.ro).
+
+Te așteptăm!
