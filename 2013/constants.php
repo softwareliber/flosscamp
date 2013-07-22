cd97bfc4e9c9cb5598d7f560031c2cdf785ea19a
Alex Cucu <alex.cucu@chevah.com>
2013-07-22 16:31:11 +0300

    Corectat an si locatie din antet.

--- 2013/constants.php
+++ 2013/constants.php
@@ -2,7 +2,7 @@
 $year = "2013";
 $wikiname = "FLOSSCamp 2013";
 $wikitagline = "Întâlnirea comunităților FLOSS din România";
-$camplocation = "26 iul - 28 iul 2012 - Undeva în România";
+$camplocation = "26 iul - 28 iul 2013 - Padina Ursului, Fundățica, Brașov, România";
 $menu = '<li><a href="../2013/" title="Acasă">Despre eveniment</a></li>' .
         '<li><a href="../2013/calea" title="Hai și tu">Cum ajungi</a></li>' .
         '<!--li><a href="../2013/membri" title="Pe cine vei întâlni">Participanți</a></li-->' .
