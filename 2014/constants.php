9321f7ee4e39e0f0c39162f1d027fe2278ea3fb3
Doru Ilasi <doru@aplix.org>
2014-07-05 15:06:41 +0300

    formular inscriere

--- 2014/constants.php
+++ 2014/constants.php
@@ -6,8 +6,8 @@ $camplocation = "24 iul - 27 iul 2014 - Șleaul Mândrului - Fundățica, Brașo
 $menu = '<li><a href="../2014/" title="Acasă">Despre eveniment</a></li>' .
         '<li><a href="../2014/calea" title="Hai și tu">Cum ajungi</a></li>' .
         '<!--li><a href="../2014/membri" title="Pe cine vei întâlni">Participanți</a></li-->' .
-        '<!--li><a href="../2014/inscriere" title="Înscriere?">Înscriere</a></li-->' .
+        '<li><a href="../2014/inscriere" title="Înscriere?">Înscriere</a></li>' .
         '<!--li><a href="../2014/poze" title="Poze">Poze</a></li-->' .
-        '<li><a href="../2014/contact" title="Scrie-ne">Contact</a></li>';
+        '<!--li><a href="../2014/contact" title="Scrie-ne">Contact</a></li-->';
 ?>
 
